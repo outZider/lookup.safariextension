c76494cde43767f7dd2af89931eb1835dcd00e5b
Yasushi Ikeda <yasushi.ikeda@gmail.com>
2010-06-11 00:44:49 +0900

    1.0.1: ・パターン定義の追加／削除を行えるようsafari.extension.settingsにpatternキーを追加した ・パターン定義に応じてコンテキストメニューを書き換えるようにした。

--- injected.js
+++ injected.js
@@ -1,14 +1,9 @@
 safari.self.addEventListener(
   "message",
   function(msg) {
-    const pattern = {
-      google: "http://translate.google.co.jp/?hl=ja&tab=wT#en|ja|{}",
-      alc: "http://eow.alc.co.jp/{}/UTF-8/?ref=sa",
-      ud: "http://www.urbandictionary.com/define.php?term={}"
-    };
     var w = document.getSelection().toString();
     if (w) {
-      safari.self.tab.dispatchMessage(pattern[msg.name].replace("{}", encodeURIComponent(w)));
+      safari.self.tab.dispatchMessage("open", {string: w, command: msg.name});
     }
   },
   false
