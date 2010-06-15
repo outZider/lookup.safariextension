d63f14f3bce80acfaaaf82ae9772aac6d6ff1550
Yasushi Ikeda <yasushi.ikeda@gmail.com>
2010-06-15 15:21:02 +0900

    1.0.10: ・GMailでタブが２つ開いてしまう不具合を解消

--- injected.js
+++ injected.js
@@ -1,9 +1,17 @@
+var lastTimeStamp = 0;
 safari.self.addEventListener(
   "message",
   function(msg) {
     var w = document.getSelection().toString();
     if (w) {
-      safari.self.tab.dispatchMessage("open", {string: w, command: msg.name});
+      switch (msg.name) {
+        case "inquiry":
+          if (msg.timeStamp > lastTimeStamp + msg.message.deltaTimeThreshold) {
+            safari.self.tab.dispatchMessage("open", {string: w, command: msg.message.command});
+          }
+          lastTimeStamp = msg.timeStamp;
+          break;
+      }
     }
   },
   false
